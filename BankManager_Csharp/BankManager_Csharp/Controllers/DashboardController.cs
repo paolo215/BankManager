65932ebdf34f615720f42ec00ae40cc746db280d
Paolo Villanueva <paolo_villanueva215@yahoo.com>
2017-07-02 17:00:32 -0700

    Added Dashboard model

--- BankManager_Csharp/BankManager_Csharp/Controllers/DashboardController.cs
+++ BankManager_Csharp/BankManager_Csharp/Controllers/DashboardController.cs
@@ -13,24 +13,89 @@ namespace BankManager_Csharp.Controllers
         public ActionResult Index()
         {
             var username = Session["username"];
-            if (username == null)
+            var is_authenticated = Session["is_authenticated"];
+            if (username == null || is_authenticated == null || (bool) is_authenticated == false)
             {
                 return RedirectToAction("Index", "Login");
             }
 
+
+
             Account account = MvcApplication.accountData[username.ToString()];
             
 
-            return View("Index", account);
+            return View("Index", new Dashboard(account, null));
         }
 
         [HttpPost]
         public ActionResult MakeTransaction(int amount, String option)
         {
-            System.Diagnostics.Debug.WriteLine(amount);
-            System.Diagnostics.Debug.WriteLine(option);
-            return View();
+            Response response = null;
+            String username = Session["username"].ToString();
+            Account account = MvcApplication.accountData[username];
+
+            if (option.Equals("Withdraw") == true)
+            {
+                response = withdraw(account, amount);
+            } else if(option.Equals("Deposit") == true)
+            {
+                response = deposit(account, amount);
+            }
+
+
+            return View("Index", new Dashboard(account, response));
         }
+
+
+        public Response withdraw(Account account, int amount)
+        {
+            int balance = account.balance;
+
+            if(balance >= amount && amount > 0)
+            {
+                account.withdraw(amount);
+                createTransaction(account, amount, "WITHDRAW");
+
+                return new Response(true, "Success! Your balance is now: " + account.balance);
+            }
+
+            return new Response(false, "Insufficient amount of founds. Your balance is currently " + account.balance);
+        }
+
+
+        public Response deposit(Account account, int amount)
+        {
+            int balance = account.balance;
+
+            if(amount > 0)
+            {
+                account.deposit(amount);
+                createTransaction(account, amount, "DEPOSIT");
+
+                return new Response(true, "Success! Your balance is now: " + account.balance);
+            }
+
+            return new Response(false, "Invalid amount of funds. Try again.");
+        }
+
+
+        public Transaction createTransaction(Account account, int amount, String status)
+        {
+            int balance = account.balance;
+            int accountId = account.accountId;
+            DateTime date = DateTime.Now;
+
+            Transaction transaction = new Transaction(MvcApplication.nextTransactionId,
+                accountId, balance, amount, status, date);
+
+            MvcApplication.transactionData[MvcApplication.nextTransactionId]= transaction;
+            account.addTransaction(transaction);
+
+            MvcApplication.nextTransactionId += 1;
+
+            return transaction;
+        }
+
     }
 
 }
---
+++ BankManager_Csharp/BankManager_Csharp/Models/Dashboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankManager_Csharp.Models
+{
+    public class Dashboard
+    {
+        public Account account { get; }
+        public Response response { get; }
+
+        public Dashboard(Account account, Response response)
+        {
+            this.account = account;
+            this.response = response;
+        }
+    }
+}
